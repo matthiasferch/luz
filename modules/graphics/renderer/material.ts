ef8cb34cab17f473a69fa7dff2ee2a083253e08a
Matthias Ferch <matthias.ferch@gmail.com>
2025-10-31 19:47:42 +0100

    Added parallax mapping

--- modules/graphics/renderer/material.ts
+++ modules/graphics/renderer/material.ts
@@ -50,6 +50,19 @@ export class Material extends Serializable {
   @Uniform()
   emissiveTexture?: Texture
 
+  @Serialize()
+  parallaxSurface: Surface | null
+
+  @Uniform()
+  parallaxTexture?: Texture
+
+  @Uniform()
+  useParallaxMapping: boolean = false
+
+  @Uniform()
+  @Serialize()
+  parallaxScale: number = 0.04
+
   @Uniform()
   @Serialize()
   opacity: number = 1.0
@@ -75,5 +88,29 @@ export class Material extends Serializable {
     if (blendMode) {
       this.blendMode = blendMode
     }
+  }
+
+  static async deserialize(data: any) {
+    const mapped: any = { ...data }
+
+    // Backward compatibility mappings
+    if (mapped.surface !== undefined && mapped.albedoSurface === undefined) {
+      mapped.albedoSurface = mapped.surface
+      delete mapped.surface
+    }
+    if (mapped.color !== undefined && mapped.albedoColor === undefined) {
+      mapped.albedoColor = mapped.color
+      delete mapped.color
+    }
+    if (mapped.texture !== undefined && mapped.albedoTexture === undefined) {
+      mapped.albedoTexture = mapped.texture
+      delete mapped.texture
+    }
+    if (mapped.heightSurface !== undefined && mapped.parallaxSurface === undefined) {
+      mapped.parallaxSurface = mapped.heightSurface
+      delete mapped.heightSurface
+    }
+
+    return (await super.deserialize(mapped)) as Material
   }
 }
